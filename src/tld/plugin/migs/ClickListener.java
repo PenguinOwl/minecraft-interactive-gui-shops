eece0f7b309c9b17d976ef8d64e10d952456cf35
BuildTools <unconfigured@null.spigotmc.org>
2017-06-24 10:30:00 -0700

    Upload

---
+++ src/tld/plugin/migs/ClickListener.java
@@ -0,0 +1,23 @@
+package tld.plugin.migs;
+import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public final class ClickListener implements Listener {
+    @EventHandler
+    public void someoneKnocked(InventoryClickEvent event) {
+        ItemStack clicked = event.getCurrentItem();
+        Inventory inventory = event.getInventory();
+        if (inventory.getName().equals(InvCtmr.$owner.getName())) { 
+            if (clicked.getType() == Material.STAINED_GLASS_PANE) { 
+                event.setCancelled(true); 
+            }
+        }
+    }
+}
---
+++ src/tld/plugin/migs/InvCtmr.java
@@ -0,0 +1,56 @@
+package tld.plugin.migs;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.*;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.*;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class InvCtmr extends JavaPlugin {
+
+	private final ClickListener clickListener = new ClickListener();
+	
+    @Override
+    public void onEnable() {
+       getConfig();
+       PluginManager pm = getServer().getPluginManager();
+       pm.registerEvents(clickListener, this);
+       PluginDescriptionFile pdfFile = this.getDescription();
+       getLogger().info( "A wild " + pdfFile.getName() + " version " + pdfFile.getVersion() + " appearerd!" );
+    }
+   
+    @Override
+    public void onDisable() {
+       saveConfig();
+    }
+   
+    public static Inventory $owner = Bukkit.createInventory(null, 45, "Owner Interface"); {
+    	for(int x = 0; x < 45; x = x + 1) {
+    		ItemStack blank = new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 15);
+    		ItemMeta cm = blank.getItemMeta();
+    		cm.setDisplayName("");
+    		blank.setItemMeta(cm);
+    		$owner.setItem(x, blank);
+    	}
+    }
+    
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args){
+        if(command.getName().equalsIgnoreCase("masterinv")){
+        	Player player = (Player) sender;
+        	player.openInventory($owner);
+            return true;
+        }
+        return false;
+    }
+	
+
+}
+
